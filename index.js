b1ef1d01442b61051290cd4fc00d51853ccfb6fe
tone4ka <toniamik@gmail.com>
2021-11-05 23:44:42 +0300

    stderr fix

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ let outputFile;
 
 for(let i = 0; i<config.length; i += 1) {
     if(config[i] === 'xx' /*повторяющиеся либо некорректные аргументы!!!!!!!!!!!!!!!! */) {
-      fs.writeSync(
-      process.stderr.fd,/*не поняла, что это("выдать соответствующий"... что-то вроде кастомной ошибки??? */
-      `111111111111111111111111111111`
-      );
-      process.exit(1)
+      process.stderr.write('there should be readable error!!!');
+      process.exit(1);
     };
   if(config[i] === '-c') cryptingConfig = config[i + 1];
   if(config[i] === '-i') inputFile = config[i + 1];
