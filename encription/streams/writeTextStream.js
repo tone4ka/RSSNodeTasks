60773e07a853d60da9606727479fff10805386ff
tone4ka <toniamik@gmail.com>
2021-11-12 23:58:35 +0300

    fix: writeStrim correction

--- encription/streams/writeTextStream.js
+++ encription/streams/writeTextStream.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const {stdout} = require('process');
+const fs = require("fs");
+const path = require("path");
+const { stdout } = require("process");
 
 function writeTextStream(outputFile) {
-    if(outputFile) {
-        const writableStream = fs.createWriteStream(outputFile);
-        return writableStream;
-    }
-    return stdout;
-};
+  if (outputFile) {
+    const writableStream = fs.createWriteStream(outputFile,{ flags: "a"});
+    return writableStream;
+  }
+  return stdout;
+}
 
 module.exports = writeTextStream;
