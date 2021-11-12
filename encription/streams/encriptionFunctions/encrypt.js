1d1e2db9520276fa93165e8a7af2aa808c6f3f5e
tone4ka <toniamik@gmail.com>
2021-11-12 23:27:42 +0300

    docs: notes correction

--- encription/streams/encriptionFunctions/encrypt.js
+++ encription/streams/encriptionFunctions/encrypt.js
@@ -6,19 +6,15 @@ function encrypt(str, encryptionMethod, encriptionDirection) {
   let res;
   switch (encryptionMethod) {
     case "C":
-      console.log("caesarCipher");
       res = caesarCipher(str, encriptionDirection);
       break;
     case "R":
-      console.log("ROT8cipher");
       res = ROT8cipher(str, encriptionDirection);
       break;
     case "A":
-      console.log("atbashCipher");
       res = atbashCipher(str);
       break;
     default:
-      console.log("default");
       break;
   }
   return res;
