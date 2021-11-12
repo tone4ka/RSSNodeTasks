a8e4f6a9f941a25c8a301f7097a6f741cca8c2fb
tone4ka <toniamik@gmail.com>
2021-11-12 23:17:41 +0300

    feat: rot8 & atbash cipher

--- encription/streams/encriptionFunctions/ROT8cipher.js
+++ encription/streams/encriptionFunctions/ROT8cipher.js
@@ -1,5 +1,20 @@
 function ROT8cipher(str, direction) {
-    
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const cipherLetters = "IJKLMNOPQRSTUVWXYZABCDEFGHijklmnopqrstuvwxyzabcdefgh";
+  return str
+    .split("")
+    .map((letter) => {
+      const index =
+        direction === 1
+          ? letters.indexOf(letter)
+          : cipherLetters.indexOf(letter);
+      if (index > -1) {
+        return direction === 1 ? cipherLetters[index] : letters[index];
+      } else {
+        return letter;
+      }
+    })
+    .join("");
 }
 
 module.exports = ROT8cipher;
--- encription/streams/encriptionFunctions/atbashCipher.js
+++ encription/streams/encriptionFunctions/atbashCipher.js
@@ -1,5 +1,17 @@
-function atbashCipher(str, direction) {
-
+function atbashCipher(str) {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const cipherLetters = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba";
+  return str
+    .split("")
+    .map((letter) => {
+      const index = letters.indexOf(letter);
+      if (index > -1) {
+        return cipherLetters[index];
+      } else {
+        return letter;
+      }
+    })
+    .join("");
 }
 
 module.exports = atbashCipher;
