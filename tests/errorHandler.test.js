042b55e203310e9a0b8523179197cb21035e9717
Antonina Zinkevich <antonina_zinkevich@epam.com>
2021-11-20 18:25:14 +0300

    feat: start test added

--- tests/errorHandler.test.js
+++ tests/errorHandler.test.js
@@ -4,6 +4,6 @@ const myError = require('../src/errors/myError');
   test('Error handler works good', () => {
     process.exit = jest.fn(()=>{});
     const spy = jest.spyOn(console, 'error').mockImplementation(() => {});  
-    errorHandler(new myError('Hello'));
-    expect(spy.mock.calls).toEqual([['Hello']]);
+    errorHandler(new myError('Error handler works good'));
+    expect(spy.mock.calls).toEqual([['Error handler works good']]);
   });
